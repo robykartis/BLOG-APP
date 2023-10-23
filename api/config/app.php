df4dd8395450e53112a7de80113d42131225d622
robykartis <robykarti.s@gmail.com>
2023-10-23 15:40:15 +0700

    Lanjut rumah

--- api/config/app.php
+++ api/config/app.php
@@ -161,7 +161,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        'Intervention\Image\ImageServiceProvider',
         /*
          * Application Service Providers...
          */
@@ -185,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Image' => 'Intervention\Image\Facades\Image'
     ])->toArray(),
 
 ];
